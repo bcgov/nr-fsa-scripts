bd134ad6dc54570de183d7df88ff815dd0973055
Anne-Marie.Bujold <anne-marie.bujold@gov.bc.ca>
2022-10-03 14:39:05 -0700

    Scripts to add datafix proxy users

---
+++ datafix/scripts/RRS-1815/FTA/ddl/Create_PROXY_FSA_FTA_DATAFIX.sql
@@ -0,0 +1,30 @@
+/*************************************************************************
+ * Thursday Sept 20, 2022                                                *
+ *                                                                       *
+ * FTA datafix proxy user 					                                     *
+ *                                                                       *
+ * This script containts statements to create PROXY_FSA_FTA_DATAFIX	     *
+ * to be used by the Datafix api to access FTA data						           *
+ *                                                                       *
+ * Notes:                                                                *
+ *                                                                       *
+ * 																																			 *
+ *************************************************************************/
+SPOOL LOGS\Create_PROXY_FSA_FTA_DATAFIX.LOG 
+DROP USER PROXY_FSA_FTA_DATAFIX CASCADE;
+
+CREATE USER PROXY_FSA_FTA_DATAFIX
+  IDENTIFIED BY &PROXY_FSA_FTA_DATAFIX
+  DEFAULT TABLESPACE USERS
+  TEMPORARY TABLESPACE TEMP
+  PROFILE NRS_NOEXPIRY_PROFILE
+  ACCOUNT UNLOCK;
+  GRANT CREATE SESSION to PROXY_FSA_FTA_DATAFIX;
+
+-- grant each of the functional roles
+
+  grant PROXY_FSA_FTA_READ_ROLE to PROXY_FSA_FTA_DATAFIX ;
+  grant PROXY_FSA_FTA_WRITE_ROLE to PROXY_FSA_FTA_DATAFIX ;
+  grant PROXY_FSA_FTA_DELETE_ROLE to PROXY_FSA_FTA_DATAFIX ;
+
+SPOOL OFF 
---
+++ datafix/scripts/RRS-1815/RRS/docs/readme.txt
@@ -0,0 +1,39 @@
+*****************************************************************************
+* SPAR - Create Proxy user in test env for rrs	                            *
+* DATE CREATED : 2022-09-20                                                 *
+* AUTHOR : CGI                                                              *
+* System : RRS								    *
+*                                                                           *
+*****************************************************************************
+
+INSTRUCTIONS:
+
+Environments include: RRSTMP1, RRSTST1, RRSPRD1
+
+=============
+RRS Updates
+============
+
+
+1) From the ddl folder, log in to database as app_rrs schema owner and execute the SET statements before running the scripts
+
+sqlplus username/password@Environment
+
+SET SCAN OFF;
+SET SQLBL ON;
+
+
+2) Run proxy user creation script
+
+start Create_PROXY_FSA_RRS_READ_ROLE.sql
+start Create_PROXY_FSA_RRS_WRITE_ROLE.sql
+
+start Create_PROXY_FSA_RRS_DATAFIX.sql
+
+
+3)  Commit the changes
+
+     commit;
+
+4) EXIT
+EXIT
