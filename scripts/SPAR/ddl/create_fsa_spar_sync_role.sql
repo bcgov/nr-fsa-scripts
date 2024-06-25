44e7c5ff2965550690f254b7aac4dab688dad001
ronrobb <5396566+ronrobb@users.noreply.github.com>
2024-06-25 10:41:08 -0700

    feat: Added sync user, role, permissions (#31)
    * Added sync user, role, permissions
    
    * fix bad commit
    
    * Update dba_artifact.yml
    
    * Update dba_artifact.yml
    
    ---------
    
    Co-authored-by: Michelle Douville <webgismd@users.noreply.github.com>

---
+++ scripts/SPAR/ddl/create_fsa_spar_sync_role.sql
@@ -0,0 +1,25 @@
+-- **************************************************************************
+-- June 24, 2024                                                    
+--                                 
+-- Forest Suite of Applications (FSA) 
+--  SPAR-774                                     
+-- New SPAR application proxy accounts                                   
+--                                                                       
+-- This script containts statements to create the proxy account role for the 
+-- SYNC module which is responsible for replicating data from postgresql
+-- to Oracle
+-- 	
+-- ***************************************************************************
+SPOOL logs\creaate_fsa_spar_sync_role.log
+
+CREATE ROLE fsa_spar_sync_role;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON the.seedlot TO fsa_spar_sync_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON the.seedlot_owner_quantity TO fsa_spar_sync_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON the.seedlot_parent_tree TO fsa_spar_sync_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON the.seedlot_parent_tree_gen_qlty TO fsa_spar_sync_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON the.seedlot_plan_zone TO fsa_spar_sync_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON the.smp_mix TO fsa_spar_sync_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON the.smp_mix_gen_qlty TO fsa_spar_sync_role;
+
+SPOOL OFF
